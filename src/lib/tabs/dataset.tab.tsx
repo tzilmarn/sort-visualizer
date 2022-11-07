fa2775b917ff87d9bed4c3b43335dab359d309e6
Salman AlSaigal <SalmanAlSaigal@gmail.com>
2022-11-08 00:40:29 +0300

    Enhance dataset tab

--- src/lib/tabs/dataset.tab.tsx
+++ src/lib/tabs/dataset.tab.tsx
@@ -1,13 +1,12 @@
 import { ComponentProps, useContext } from 'react'
 import SimulationControlContext from '../simulation.context'
-import { IoAdd as PlusIcon, IoRemove as MinusIcon } from 'react-icons/io5'
 
 type Props = {}
 
 const Button = (props: ComponentProps<'button'>) => (
 	<button
 		{...props}
-		className={`flex items-center justify-center rounded-lg bg-gray-800/30 text-center text-4xl [flex-grow:1] active:bg-indigo-800/80 disabled:bg-gray-800/10 disabled:text-gray-500 ${props.className}`}
+		className={`flex aspect-square items-center justify-center rounded-lg bg-gray-800/30 p-2 text-center text-xl font-black tracking-tighter text-gray-200 [flex-grow:1] active:bg-indigo-800/80 disabled:bg-gray-800/10 disabled:text-gray-500 ${props.className}`}
 	/>
 )
 
@@ -15,43 +14,35 @@ export default function DatasetTab({}: Props) {
 	const { datasetSize, setDatasetSize } = useContext(SimulationControlContext)
 
 	return (
-		<div className='flex gap-2'>
-			<div className='flex flex-col gap-1 [flex-grow:1]'>
-				<Button
-					disabled={datasetSize <= 0}
-					onClick={() => setDatasetSize(datasetSize - 5)}
-					className='[flex-grow:2]'
-				>
-					<MinusIcon /> 5
-				</Button>
-				<Button
-					disabled={datasetSize <= 0}
-					onClick={() => setDatasetSize(datasetSize - 1)}
-					className='[flex-grow:1]'
-				>
-					<MinusIcon />
-				</Button>
-			</div>
-			<figure className='group flex flex-col items-center justify-center rounded-lg border-2 border-gray-300 p-4 text-center text-gray-300 md:[flex-grow:3]'>
+		<div className='grid grid-flow-col grid-cols-[1fr_2fr_1fr] grid-rows-2 gap-2'>
+			<Button
+				disabled={datasetSize <= 0}
+				onClick={() => setDatasetSize(datasetSize - 5)}
+			>
+				˗5
+			</Button>
+			<Button
+				disabled={datasetSize <= 0}
+				onClick={() => setDatasetSize(datasetSize - 1)}
+			>
+				˗1
+			</Button>
+			<figure className='row-span-2 flex flex-col items-center justify-center rounded-lg p-4 text-center text-gray-300'>
 				<span className='text-5xl font-black'>{datasetSize}</span>
 				<figcaption className='text-md text-2xl font-bold'>Bars</figcaption>
 			</figure>
-			<div className='flex flex-col gap-1 [flex-grow:1]'>
-				<Button
-					disabled={datasetSize >= 100}
-					onClick={() => setDatasetSize(datasetSize + 5)}
-					className='[flex-grow:2]'
-				>
-					<PlusIcon /> 5
-				</Button>
-				<Button
-					disabled={datasetSize >= 100}
-					onClick={() => setDatasetSize(datasetSize + 1)}
-					className='[flex-grow:2]'
-				>
-					<PlusIcon />
-				</Button>
-			</div>
+			<Button
+				disabled={datasetSize >= 100}
+				onClick={() => setDatasetSize(datasetSize + 5)}
+			>
+				﹢5
+			</Button>
+			<Button
+				disabled={datasetSize >= 100}
+				onClick={() => setDatasetSize(datasetSize + 1)}
+			>
+				﹢1
+			</Button>
 		</div>
 	)
 }
